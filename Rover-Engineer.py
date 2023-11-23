d101ea2526aec582f47c54a182c1d2c0f7d826d3
SIR_GEO <g.miller@millerlog.net>
2023-11-23 14:51:17 +0000

    Update to see if works with MS Azure

--- Rover-Engineer.py
+++ Rover-Engineer.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import openai
-from openai import OpenAI
-import openai
 import time
 import keychain
 
@@ -13,48 +11,55 @@ client = openai.Client()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 @app.route('/rover_engineer_request', methods=['POST'])
 def rover_engineer_request():
-    data = request.json
-    user_question = data['question']
+    try:
+        data = request.json
+        user_question = data['question']
 
-    # Create a thread
-    thread = client.beta.threads.create()
+        # Create a thread
+        thread = client.beta.threads.create()
 
-    # Add a message to the thread with the user's question
-    message = client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=user_question
-    )
-
-    # Create a run
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id="asst_X6pCppPwljfx0SJwFfpyF1lS"
-    )
-
-    # Polling for the run's completion
-    while True:
-        run_status = client.beta.threads.runs.retrieve(
+        # Add a message to the thread with the user's question
+        message = client.beta.threads.messages.create(
             thread_id=thread.id,
-            run_id=run.id
+            role="user",
+            content=user_question
         )
-        if run_status.status == 'completed':
-            break
-        time.sleep(1)  # Wait for 1 second before polling again
 
-    # Retrieve messages after the run is completed
-    thread_messages = client.beta.threads.messages.list(thread.id)
+        # Create a run
+        run = client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id="asst_X6pCppPwljfx0SJwFfpyF1lS"
+        )
 
-    # Find and return the assistant's response
-    for msg in thread_messages.data:
-        if msg.role == 'assistant':
-            return jsonify({'response': msg.content[0].text.value})
+        # Polling for the run's completion
+        for _ in range(30):  # Poll for up to 30 seconds
+            run_status = client.beta.threads.runs.retrieve(
+                thread_id=thread.id,
+                run_id=run.id
+            )
+            if run_status.status == 'completed':
+                break
+            time.sleep(1)
 
-    return jsonify({'response': 'No response from the AI.'})
+        # Retrieve messages after the run is completed
+        thread_messages = client.beta.threads.messages.list(thread.id)
+
+        # Find and return the assistant's response
+        for msg in thread_messages.data:
+            if msg.role == 'assistant':
+                return jsonify({'response': msg.content[0].text.value})
+
+        return jsonify({'response': 'No response from the AI.'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)  # Turn off debug mode for production
