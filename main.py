7d934e02e6692ab7e3510bc57eb2efcfca33fd25
SIR-GEO <g.miller@millerlog.net>
2023-11-23 20:04:51 +0000

    asynchronous operations and removed local main

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ import asyncio
 import os
 import logging
 
-
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+# Ensure that the OPENAI_API_KEY is set in the environment variables.
 openai.api_key = os.environ.get('OPENAI_API_KEY')
+if not openai.api_key:
+    raise ValueError("The OPENAI_API_KEY environment variable must be set.")
+
 client = openai.Client()
 
 logger = logging.getLogger("uvicorn.error")
@@ -20,8 +24,8 @@ async def index(request: Request):
     try:
         return templates.TemplateResponse("index.html", {"request": request})
     except Exception as e:
-        logger.error(f"Error in index endpoint: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+        logger.exception("Error in index endpoint")
+        raise HTTPException(status_code=500, detail="An error occurred while processing your request.")
 
 class RoverEngineerRequest(BaseModel):
     question: str
@@ -31,21 +35,22 @@ async def rover_engineer_request(data: RoverEngineerRequest):
     try:
         user_question = data.question
 
-        thread = client.beta.threads.create()
-        message = client.beta.threads.messages.create(
+        # Assuming that the OpenAI client has asynchronous methods.
+        thread = await client.beta.threads.create()
+        message = await client.beta.threads.messages.create(
             thread_id=thread.id, role="user", content=user_question)
 
-        run = client.beta.threads.runs.create(
+        run = await client.beta.threads.runs.create(
             thread_id=thread.id, assistant_id="asst_X6pCppPwljfx0SJwFfpyF1lS")
 
         for _ in range(30):
-            run_status = client.beta.threads.runs.retrieve(
+            run_status = await client.beta.threads.runs.retrieve(
                 thread_id=thread.id, run_id=run.id)
             if run_status.status == 'completed':
                 break
             await asyncio.sleep(1)
 
-        thread_messages = client.beta.threads.messages.list(thread.id)
+        thread_messages = await client.beta.threads.messages.list(thread.id)
 
         for msg in thread_messages.data:
             if msg.role == 'assistant':
@@ -54,9 +59,5 @@ async def rover_engineer_request(data: RoverEngineerRequest):
         return {'response': 'No response from the AI.'}
 
     except Exception as e:
-        logger.error(f"Error in rover_engineer_request endpoint: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+        logger.exception("Error in rover_engineer_request endpoint")
+        raise HTTPException(status_code=500, detail="An error occurred while processing your request.")
