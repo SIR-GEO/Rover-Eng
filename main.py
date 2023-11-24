6ae1bc8a80e042c4acd8e7ac8a6af7ecd13d07f4
SIR_GEO <g.miller@millerlog.net>
2023-11-24 17:10:17 +0000

    removed development bit

--- main.py
+++ main.py
@@ -71,5 +71,5 @@ async def rover_engineer_request(data: RoverEngineerRequest):
     
 
 # Comment out if wanting to deploy on MS Azure:
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+#if __name__ == "__main__":
+   # uvicorn.run(app, host="127.0.0.1", port=8000)
