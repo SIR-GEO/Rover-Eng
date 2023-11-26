cec4b757e8976bac61268582616485ab8a578ca8
SIR-GEO <g.miller@millerlog.net>
2023-11-26 21:30:14 +0000

    added chat history. remembers previous response

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ client = openai.Client()
 
 logger = logging.getLogger("uvicorn.error")
 
+# Global variable to store the thread id
+current_thread_id = None
+
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     try:
@@ -39,37 +42,54 @@ class RoverEngineerRequest(BaseModel):
 
 @app.post("/rover_engineer_request")
 async def rover_engineer_request(data: RoverEngineerRequest):
+    global current_thread_id
     try:
         user_question = data.question
 
-        # Create the thread, message, and run synchronously
-        thread = client.beta.threads.create()
-        message = client.beta.threads.messages.create(
-            thread_id=thread.id, role="user", content=user_question)
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id, assistant_id="asst_X6pCppPwljfx0SJwFfpyF1lS")
+        # If there's no current thread, create one
+        if current_thread_id is None:
+            thread = client.beta.threads.create()
+            current_thread_id = thread.id
+        else:
+            thread = client.beta.threads.retrieve(thread_id=current_thread_id)
 
-        # Use a synchronous loop with time.sleep()
+        # Add the user's question to the thread
+        message = client.beta.threads.messages.create(
+            thread_id=current_thread_id, role="user", content=user_question)
+
+        # Run the AI assistant
+        run = client.beta.threads.runs.create(
+            thread_id=current_thread_id, assistant_id="asst_X6pCppPwljfx0SJwFfpyF1lS")
+
+        # Wait for the run to complete
         for _ in range(120):
             run_status = client.beta.threads.runs.retrieve(
-                thread_id=thread.id, run_id=run.id)
+                thread_id=current_thread_id, run_id=run.id)
             if run_status.status == 'completed':
                 break
-            time.sleep(0.5)  # Sleep for 0.5 seconds
+            time.sleep(0.5)
 
-        thread_messages = client.beta.threads.messages.list(thread.id)
+        # Retrieve all messages in the thread
+        thread_messages = client.beta.threads.messages.list(current_thread_id)
 
+        # Find the AI's response
+        ai_response = None
         for msg in thread_messages.data:
             if msg.role == 'assistant':
-                return {'response': msg.content[0].text.value}
+                ai_response = msg.content[0].text.value
+                break
+
+        if ai_response:
+            # Log the AI's response instead of creating a system message
+            logger.info(f"AI Response: {ai_response}")
+            return {'response': ai_response}
 
         return {'response': 'No response from the AI.'}
-
     except Exception as e:
         logger.exception("Error in rover_engineer_request endpoint")
         raise HTTPException(status_code=500, detail="An error occurred while processing your request.")
     
 
 # Comment out if wanting to deploy on MS Azure:
-#if __name__ == "__main__":
-   # uvicorn.run(app, host="127.0.0.1", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
